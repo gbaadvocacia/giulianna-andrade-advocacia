b235a55d7014a68c61bd41ff7bec27d2527d65f4
Paula Nader <paulampaan@gmail.com>
2022-06-08 17:59:55 -0300

    Fixing icon padding

--- src/components/ValuesCard/index.tsx
+++ src/components/ValuesCard/index.tsx
@@ -5,17 +5,15 @@ interface IValuesProps {
     value: string;
     type: string;
 }
-const ValuesCard: React.FC<IValuesProps> = ({ value, type }) => {
-    return (
-        <Border className="d-flex flex-sm-column align-items-center justify-content-center w-100 p-4">
-            <div className="flex-sm-grow-1 d-flex align-items-center">
-                <img src={value} alt="Values image" className="img-fluid" />
-            </div>
-            <div className="text-center mt-sm-3">
-                <h5 className="card-title mt-auto mb-0 fw-bold">{type}</h5>
-            </div>
-        </Border>
-    );
-};
+const ValuesCard: React.FC<IValuesProps> = ({ value, type }) => (
+    <Border className="d-flex flex-sm-column align-items-center justify-content-center w-100 p-4">
+        <div className="flex-sm-grow-1 d-flex align-items-center p-0 px-sm-4">
+            <img src={value} alt="Values image" className="img-fluid" />
+        </div>
+        <div className="text-center mt-sm-3">
+            <h5 className="card-title mt-auto mb-0 fw-bold">{type}</h5>
+        </div>
+    </Border>
+);
 
 export default ValuesCard;
